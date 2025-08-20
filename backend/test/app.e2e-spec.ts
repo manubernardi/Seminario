ee05204395f702135777c7add90e6bed9a556561
manubernardi <manuber29@gmail.com>
2025-08-20 17:11:48 -0300

    Frontend anana ananashe

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -15,11 +15,11 @@ describe('AppController (e2e)', () => {
     app = moduleFixture.createNestApplication();
     await app.init();
   });
-
+/*
   it('/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
       .expect('Hello World!');
   });
-});
+});*/})
