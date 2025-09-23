ef64fb299520ca49179110ef2a5428be6e4a5a14
manubernardi <manuber29@gmail.com>
2025-09-23 13:42:15 -0300

    Avanzo en el frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
+import { Stock } from './pages/stock/stock'
+import { Ventas } from './pages/ventas/ventas'
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     { path: 'login', component: Login },
     { path: 'home', component: Home },
-    { path: 'register', component: Register }
+    { path: 'register', component: Register },
+    {path: 'stock', component: Stock},
+    {path: 'ventas', component: Ventas},
 ];
+
