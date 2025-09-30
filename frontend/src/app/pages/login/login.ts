02148a289af08f34d57da51208b6daa5a1730233
Santiago Falvo <falvosanti@gmail.com>
2025-09-30 14:40:25 -0300

    Página de login y de incio

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,14 +1,16 @@
 
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
+
 
 
 @Component({
   selector: 'app-login',
   imports: [
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    FormsModule
   ],
   templateUrl: './login.html',
   styleUrl: './login.css'
@@ -23,12 +25,26 @@ export class Login {
     private router: Router
   ){
       this.loginForm = this.fb.group({
-          email: ['', [Validators.required, Validators.email]],
-          password: ['', [Validators.required, Validators.minLength(6)]]
+          dni: ['', [Validators.required, 
+                    Validators.minLength(7), 
+                    Validators.maxLength(8),
+                    Validators.pattern(/^\d+$/)]],
+          password:['', [Validators.required, 
+                        Validators.minLength(6), 
+                        Validators.maxLength(20)]]
       });
   }
 
-  enviar(){
+  ngOnInit(){
+    const esSupervisor = document.getElementById('supervisor-check');
+    const formSupervisor = document.getElementById('supervisor');
+    esSupervisor?.addEventListener('click', ()=>{
+      console.log('obtiene el elemento')
+      formSupervisor?.classList.toggle('unchecked');
+    })
+  }
+
+  public enviar(){
     if (this.loginForm.invalid) {
       this.error = 'Completa todos los campos correctamente.';
       return;
@@ -38,4 +54,5 @@ export class Login {
 
     this.router.navigate(['/home']);
   }
+
 }
