0106543ec5dde71f90fe7be7675daf034393dcc4
augustosan-182 <augustosan7@gmail.com>
2025-10-02 15:39:28 -0300

    corrigo errores ESTOY BIEN IDIOTA PETER PARKER :v

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection} from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
